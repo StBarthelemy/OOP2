5d4e7e1b39108aad3bc1336a9b0ed0fa3b94d616
Vadim <vip-vadim_2010@mail.ru>
2023-12-01 16:00:36 +0300

    save

---
+++ dz/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Human human1 = new Human("Вадим");
+        Human human2 = new Human("Екатерина");
+        market.acceptToMarket(human1);
+        market.acceptToMarket(human2);
+        market.update();
+    }
+}
